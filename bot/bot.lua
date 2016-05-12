bfe315f34578bd600ba20628ade9c537a9d698f1
mobinlord <mobinlord29@gmail.com>
2016-05-12 17:04:15 +0430

    Update and rename seedbot.lua to bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -226,30 +226,20 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {219339258,198794027,175636120},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[Shadowking v1 - Open Source
+https://github.com/mobinlord/shadow-master-super
 
-https://github.com/SEEDTEAM/TeleSeed
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
+@Thisispouria (Founder)
+@It_Is_Crazy (Developer)
+@SHO_OKER_ADMIN (Sudo)
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+@shadowking_CH
 ]],
     help_text_realm = [[
 Realm Commands:
